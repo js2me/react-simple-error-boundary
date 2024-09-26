e3b1fc7e29d2236d17fcf01e19a496c287a31212
Sergey Volkov <js2me@outlook.com>
2024-09-26 10:51:16 +0300

    fix: exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
+  clean: true,
   dts: true,
   entry: ['src/index.ts'],
   format: ['esm'],
@@ -13,4 +14,5 @@ export default defineConfig({
   treeshake: {
     preset: 'recommended',
   },
+  esbuildPlugins: [],
 });
